f36eda8b25b80b0df526e9534e8285b4eeaabab3
Maru de Vera <maru@sheepd.com>
2019-05-30 02:49:42 +0800

    more stuff

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>Hello {props.name}! Welcome to docker</div>
 )
 
 Hello.defaultProps = {
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="Marvs" />,
+    <Hello name="Emman" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
